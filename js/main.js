c2e06dacf13f357e3bbb73894276d13a543308bc
Caroline Rozali <UnorthodoxThing@Outlook.com>
2018-12-15 21:44:04 +1100

    Update main.js
    feat: new feature
    
    Same functionality, however, different names. Usually ending with 'two' is the new component and option user can now interact with.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 
-const catImage = document.querySelector('img');
-const clickContainer = document.getElementById('click-counted');
-let countClicks = 0;
-catImage.addEventListener('click', function(){
-  countClicks++;
-  clickContainer.innerHTML = countClicks;
+const catImageOne = document.querySelector('.cat-one');
+const catImageTwo = document.querySelector('.cat-two');
+const clickContainerOne = document.getElementById('image-one-counted');
+const clickContainerTwo = document.getElementById('image-two-counted');
+let countClicksOne = 0;
+let countClicksTwo = 0;
+catImageOne.addEventListener('click', function(){
+  countClicksOne++;
+  clickContainerOne.innerHTML = countClicksOne;
+}, false);
+
+catImageTwo.addEventListener('click', function(){
+  countClicksTwo++;
+  clickContainerTwo.innerHTML = countClicksTwo;
 }, false);
